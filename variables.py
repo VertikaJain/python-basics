04564292dc6f56dfb87230a80de09145a99f1e0c
Vertika Jain <vertikajain96@gmail.com>
2021-01-19 15:27:33 +0530

    Variables basic

--- variables.py
+++ variables.py
@@ -13,3 +13,25 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+x = 1
+name = "abc"
+pie = 3.14
+isEmployeed = True
+print(x)
+
+# Multiple assignment
+x, name, pie, isEmployed = (2, "xyz", 3.13, False)
+print(x, name, pie, isEmployed)
+
+print(type(x))
+
+# Casting..
+x = str(x)
+print(type(x))
+
+y = int(pie)
+print(type(y), y)
+
+z = float(y)
+print(type(z), z)  # 3.0 and not 3.13
