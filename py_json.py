561920831e8b8ced7407dbd28ac08bae30301aac
Vertika Jain <vertikajain96@gmail.com>
2021-01-20 16:00:28 +0530

    JSON loads and dumps methods.

--- py_json.py
+++ py_json.py
@@ -1,1 +1,20 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+import json
+
+userJSON = '{ "name" : "vertika", "age" : 25}'  # create JSON
+
+user = json.loads(userJSON)  # returns a dictionary
+print(user)
+print(type(user))
+print(user["name"])
+
+# create json from dictionary
+company = {
+    'name': 'amazon',
+    'location': 'bangalore'
+}
+
+companyJSON = json.dumps(company)
+print(companyJSON)
+print(type(companyJSON)) # string/json
+print(type(company)) # dictionary
